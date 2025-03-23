1c4c00f3256d4fe80b29a7af5843c42f65d64383
ronim <roni.minkkinen@gmail.com>
2025-03-23 03:07:19 +0200

    2 tehtävä korjattu

--- Viikko 1/2.py
+++ Viikko 1/2.py
@@ -2,5 +2,7 @@ import numpy as np
 
 a = 1.6
 b = 2.3
-c = np.sqrt((int(a)**2) + (int(b)**2))
-print(c)
+a_pot = a**2
+b_pot = b**2
+c = np.sqrt(a_pot+b_pot)
+print(f"Suorakulmion joiden sivujen pituus on {a} ja {b} metriä, hypotenuusan pituus on {c:6.2f} metriä")
